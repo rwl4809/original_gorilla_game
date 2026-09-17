9ec07ba2f43230acbfcd320e2c3595b7a185d729
rwl4809 <rwl4809@g.rit.edu>
2026-09-17 10:07:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -352,7 +352,7 @@ function draw() {
 function drawBackgroundSky() {
   const gradient = ctx.createLinearGradient(0, 0, 0, window.innerHeight);
   if (settings.mode === "dark") {
-    gradient.addColorStop(1, "#27507F");
+    gradient.addColorStop(1, "#F8BBDO");
     gradient.addColorStop(0, "#F8BBDO");
   } else {
     gradient.addColorStop(1, "#F8BA85");
